c3f8a22780fcb315738aabef151afe97c7094cd1
SanclerZanella <>
2022-06-07 23:39:21 +0000

    Set debug to false

--- run.py
+++ run.py
@@ -9,4 +9,4 @@ app = create_app(Config)
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
